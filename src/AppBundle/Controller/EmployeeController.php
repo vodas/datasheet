edf3ce2803371224c692a2f80ee457d29b76018f
Michal Wejwoda <wejwoda@darmol.pl>
2016-12-07 13:51:09 +0100

    changes in displaying

---
+++ src/AppBundle/Controller/EmployeeController.php
@@ -0,0 +1,38 @@
+<?php
+namespace AppBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use FOS\UserBundle\Doctrine\UserManager;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Matches /employee/
+     *
+     * @Route("/employee/", name="employee_index")
+     */
+    public function indexAction()
+    {
+        $userManager = $this->get('fos_user.user_manager');
+        $users = $userManager->findUsers();
+
+
+        return $this->render('employee.html.twig', array( 'users' => $users
+        ));
+    }
+
+    /**
+     * Matches /employee/*
+     *
+     * @Route("/employee/{userid}", name="employee_employee")
+     */
+    public function employeeAction($userid)
+    {
+        $userManager = $this->get('fos_user.user_manager');
+        $users = $userManager->findUsers();
+
+
+        return $this->render('employee.html.twig', array( 'users' => $users
+        ));
+    }
+}
